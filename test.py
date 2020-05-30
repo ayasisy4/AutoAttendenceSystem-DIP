86ea77f11b80eec0e922cc4437210655ffea9802
aya elsisy <ayasisy4@gmail.com>
2020-05-31 00:26:18 +0200

    make a vedio recgonitiona dn excel filling

--- test.py
+++ test.py
@@ -2,6 +2,15 @@ from cv2 import cv2
 import os 
 import numpy as np 
 import facedetection as fr 
+import csv
+import pandas as pd
+from time import sleep
+from datetime import date
+
+today = date.today()
+
+# dd/mm/YY
+d1 = today.strftime("%d/%m/%Y")
 
 testImage = cv2.imread('/Users/Aya/AutoAttendenceSystem-DIP/TestImages/alluni.jpg')
 detectedFaces, grayImage = fr.facedetetection(testImage)
@@ -27,25 +36,103 @@ facerecongizer.save('trainingData.yml')
 
 # facerecongizer= cv2.face.LBPHFaceRecognizer_create()
 # facerecongizer.read('/Users/Aya/AutoAttendenceSystem-DIP/trainingData.yml')
-name={0: "rawda",
+namemanual={0: "rawda",
 1: "yasmeen",
 2: "sohaila",
 3:"aya"}
+
+filename = 'E:/backendData.xlsx'
+
+names = {}
+labels = []
+students = []
+finalrows=[]
+# function to deal with excel 
+def  fromExcelToCsv():
+    df = pd.read_excel(filename,index = False)
+    df.to_csv('./data.csv')
+
+def dataGetter():
+    with open('data.csv','r') as f:
+        data = csv.reader(f)
+        next(data) # to get first rowwith names 
+        rows = list(data)
+        for row in rows:
+            names[int(row[0])] =row[1]
+
+def attended(studentName):
+    with open('data.csv','r') as f :
+        data = csv.reader(f)
+        row0 = next(data)
+        row0.append(d1)
+        print(row0)
+        rows = list(data)
+        print('this is all data before any modi',rows)
+       
+
+        for row in rows:
+            print("row name ,", row[1])
+            if row[1] == studentName:
+                row.append('1')
+                finalrows.append(row)
+                print("row after appednig attendce ,", row)
+            else:
+                row.append('0')
+                     
+
+                
+            
+        # totalrows = set(c)
+        print('this is fina data',finalrows)
+
+def writeincsv (latestdata): 
+        with open('data.csv','w') as g:
+            writer = csv.writer(g,lineterminator='\n')
+            writer.writerows(latestdata)
+                    
+def updateExcel():
+        with open('data.csv') as f:
+            data = csv.reader(f)
+            lines = list(data)
+            for line in lines:
+                line.pop(0)
+            with open('data.csv','w') as g:
+                writer = csv.writer(g,lineterminator='\n')
+                writer.writerows(lines)
+                
+        df = pd.read_csv('data.csv')
+        df.to_excel(filename,index = False)
+
+
+fromExcelToCsv() # converting the excel to csv for use
+dataGetter() # getting the data from csv in a dictionary
+print('Total students :',names)
 
 for face in detectedFaces:
     (x,y,w,h)= face
     roi_gray= grayImage[y:y+h,x:x+h]
     label,confidence= facerecongizer.predict(roi_gray)
-    print("confidnece: ", confidence)
-    print("label: ", label)
+    # print("confidnece: ", confidence)
+    # print("label: ", label)
     fr.draw_rect(testImage,face)
-    predictedName= name[label]
-    if(confidence < 90 ):
+    predictedName= namemanual[label]
+    if(confidence < 117 ):
         continue
     fr.put_text(testImage,predictedName,x,y)
+    labels.append(label)
+    students.append(namemanual[label])
+    totalstudents = set(students)
+    justlabels = set(labels)
+    print('student Recognised : ',students,totalstudents,justlabels)
+for i in justlabels:
+    print('label count : ', labels.count(i))
+    print("names of i is ", names[i])
+    attended(names[i])
 
+
+writeincsv(finalrows)
 resized_img= cv2.resize(testImage,(1200,1200))
-
+# updateExcel()
 cv2.startWindowThread()
 cv2.namedWindow("preview")
 cv2.imshow("face detection result", resized_img)
